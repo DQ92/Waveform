cd3dd0eaf585e5278d85da3bcff175a54d328b2a
Robert Mietelski <mietelski.robert@gmail.com>
2018-09-03 10:03:49 +0200

    Removed not used files.

--- Waveform/ViewController.swift
+++ Waveform/ViewController.swift
@@ -14,11 +14,8 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet var record_btn_ref: UIButton!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var totalTimeLabel: UILabel!
-    @IBOutlet weak var waveform: WaveformViewScroll!
-    @IBOutlet weak var waveformRightConstraint: NSLayoutConstraint!
     @IBOutlet weak var collectionViewWaveform: WaveformView!
     @IBOutlet weak var collectionViewRightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var timerLabel: UILabel!
 
     // MARK: - Private Properties
     let preferredTimescale: CMTimeScale = 1000
