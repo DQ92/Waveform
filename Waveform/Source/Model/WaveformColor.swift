52cf7b61a23e1321d43cf85be0db4fbd4fe34348
Michał Kos <awesomek.mk@gmail.com>
2018-09-19 11:15:00 +0200

    :pencil: :lipstick: Changes in waveform colors.

--- Waveform/Source/Model/WaveformColor.swift
+++ Waveform/Source/Model/WaveformColor.swift
@@ -4,15 +4,16 @@ import UIKit
 
 class WaveformColor {
     static func color(for mode: RecordingMode) -> UIColor {
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0.5
+        var red: CGFloat = 200/255
+        var green: CGFloat = 200/255
+        var blue: CGFloat = 200/255
         switch mode {
         case .normal:
             break
         case .override(let turn):
-            red = CGFloat(turn) * 40.0 / 255.0
-            green = 1
+            red = 240 / 255
+            green = 0
+            blue = 0
         }
 
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
