e5be851eaac8f6b60a2fce4c70a5a38b7da7a50c
Michał Kos <awesomek.mk@gmail.com>
2018-09-20 09:35:46 +0200

    :lipstick: Add zoom in and zoom out buttons.

--- Waveform/Source/Loader/FileDataLoader.swift
+++ Waveform/Source/Loader/FileDataLoader.swift
@@ -14,57 +14,57 @@ class FileDataLoader {
     private var fileReference: ExtAudioFileRef?
     private var audioFormat = AudioUtils.monoFloatNonInterleavedFormat(with: AudioUtils.defaultSampleRate)
     private var fileLengthInFrames: Int?
-    
+
     // MARK: - Public properties
-    
+
     var fileDuration: TimeInterval!
 
     // MARK: - Initialization
 
     init(fileURL: URL) throws {
         self.fileURL = fileURL
-        
+
         try setup()
     }
-    
+
     convenience init(fileName: String, fileFormat: String) throws {
         guard let filePathString = Bundle.main.path(forResource: fileName, ofType: fileFormat),
-            let url = URL(string: filePathString) else {
-                throw FileDataLoaderError.pathOrFormatProvidedInvalid
+              let url = URL(string: filePathString) else {
+            throw FileDataLoaderError.pathOrFormatProvidedInvalid
         }
-        
+
         try self.init(fileURL: url)
     }
-    
+
     // MARK: - Private methods
-    
+
     private func setup() throws {
         try openFile(with: fileURL)
     }
-    
+
     private func openFile(with fileURL: URL) throws {
         guard let sourceUrl = fileURL as CFURL? else {
             throw FileDataLoaderError.providedURLNotAcceptable
         }
-        
+
         if ExtAudioFileOpenURL(sourceUrl, &fileReference) != noErr {
             throw FileDataLoaderError.openUrlFailed
         }
-        
+
         let fileLengthInFrames = try getFileLengthInFrames(for: fileReference!)
         self.fileLengthInFrames = fileLengthInFrames
-        
+
         if ExtAudioFileSetProperty(fileReference!,
                                    kExtAudioFileProperty_ClientDataFormat,
                                    UInt32(MemoryLayout<AudioStreamBasicDescription>.size),
                                    &audioFormat) != noErr {
             throw FileDataLoaderError.setFormatFailed
         }
-        
+
         let duration: TimeInterval = Double(fileLengthInFrames) / audioFormat.mSampleRate
         self.fileDuration = duration
     }
-    
+
     private func getFileLengthInFrames(for fileReference: ExtAudioFileRef) throws -> Int {
         var fileLengthInFrames: Int = 0
         var thePropertySize = UInt32(MemoryLayout.stride(ofValue: fileLengthInFrames))
@@ -72,13 +72,13 @@ class FileDataLoader {
                                    kExtAudioFileProperty_FileLengthFrames,
                                    &thePropertySize,
                                    &fileLengthInFrames) != noErr {
-            throw FileDataLoaderError.retriveFileLenghtFailed
+            throw FileDataLoaderError.retrieveFileLengthFailed
         }
         return fileLengthInFrames
     }
 
     // MARK: - Access methods
-    
+
     func loadFile(completion: (_ fileFloatArray: [Float]) -> Void) throws {
         let numberOfPoints = Int(Double(WaveformConfiguration.microphoneSamplePerSecond) * fileDuration)
         let framesPerBuffer = UInt32(fileLengthInFrames! / numberOfPoints)
@@ -96,14 +96,14 @@ class FileDataLoader {
 
         for _ in 0..<numberOfPoints {
 
-             var bufferSize = UInt32(framesPerBuffer)
+            var bufferSize = UInt32(framesPerBuffer)
 
             if ExtAudioFileRead(fileReference!,
-                    &bufferSize,
-                    &bufferList) != noErr {
+                                &bufferSize,
+                                &bufferList) != noErr {
                 throw FileDataLoaderError.fileReadFailed
             }
-    
+
             var monoSamples = [Float]()
             let ptr = bufferList.mBuffers.mData?
                                          .assumingMemoryBound(to: Float.self)
